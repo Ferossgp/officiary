b856418e6067f8a7bbd43b4500c806f6a50acdf3
Gheorghe Pinzaru <feross95@gmail.com>
2019-10-27 07:05:16 +0300

    Add prod build to repo
    :scream:

---
+++ build/precache-manifest.1af5151b906cce125d8d8dd7f12349eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74c3408dc6ada315b286fcdf9cd1b0fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80de9be5c8b4f90c8547",
+    "url": "/static/css/main.a6604750.chunk.css"
+  },
+  {
+    "revision": "e6df4bd2d8e439c28da5",
+    "url": "/static/js/2.139a4e22.chunk.js"
+  },
+  {
+    "revision": "80de9be5c8b4f90c8547",
+    "url": "/static/js/main.135206ac.chunk.js"
+  },
+  {
+    "revision": "0f33392b71b273d07461",
+    "url": "/static/js/runtime-main.75f19c14.js"
+  }
+]);
